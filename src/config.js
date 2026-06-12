2c88acae3711dbb659d9976b7c73d705fbd510a5
Lithun Munasinghe <lithunimunasinghe1@gmail.com>
2026-06-12 09:13:43 +0530

    Revert "change the base url"
    This reverts commit 35eb8105509e02c885f246d10420afef0b2a74ba.

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://clothingpos.dockyardsoftware.com/';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://testrcc.dockyardsoftware.com';
